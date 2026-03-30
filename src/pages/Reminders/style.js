cdbac713652a1f0e6c2b9f7dd0e9cc8449f17093
BiellNonato <gabrielnonato53@gmail.com>
2026-03-30 16:42:04 -0300

    feat: font family Reminders

--- src/pages/Reminders/style.js
+++ src/pages/Reminders/style.js
@@ -19,10 +19,13 @@ export const ContentTitles = styled.View`
 
 export const Title = styled.Text`
 font-size: 20px;
+font-family: 'Poppins_600SemiBold';
 `;
 
 export const SubTitle = styled.Text`
 font-size: 14px;
+font-family: 'Poppins_400Regular';
+
 `;
 
 export const SectionUser = styled.View`
@@ -34,32 +37,38 @@ gap: 7px;
 export const UserName = styled.Text`
 font-size: 14px;
 align-self: center;
+font-family: 'Poppins_300Light';
 `;
 
 export const Reminder = styled.View`
 flex-direction: row;
 align-items: center;
-padding: 42px;
+padding-left: 31px;
 gap: 10px;
 `;
 
 export const RemindersTitle = styled.Text`
-
+font-size: 20px;
+font-family: 'Poppins_500Medium';
+margin-top: 5px;
 `;
 
 export const ContainerCollect = styled.View`
 padding: 43px 31px;
 gap: 30px;
+padding-right: 130px;
 `;
 
 export const ContainerTruck = styled.View`
 flex-direction: row;
 gap: 16px;
 align-items: center;
+
 `;
 
 export const CollectText = styled.Text`
 font-size: 16px;
+font-family: 'Poppins_400Regular';
 `;
 
 export const ContainerGameMaria = styled.View`
@@ -68,20 +77,25 @@ gap: 16px;
 `;
 
 export const TextGameMaria = styled.Text`
-
+font-size: 16px;
+font-family: 'Poppins_400Regular';  
 `;
 
 export const LinkGameMaria = styled.Text`
 font-size: 19px;
 color: #FF00C8;
+font-family: 'Poppins_500Medium';
 `;
 
 export const ContentLinkGameMaria = styled.View`
 flex-direction: row;
 align-self: flex-end;
 padding: 0px 43px;
+padding-right: 80px;
 align-items: center;
 gap: 8px;
+font-family: 'Poppins_400Regular';
+font-weight: 400;
 `;
 
 export const WavesContainer = styled.View`
