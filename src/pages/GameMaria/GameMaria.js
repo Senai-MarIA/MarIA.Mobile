e4c8a541157f64893b6643489c668139d9366a78
BiellNonato <Gabrielnonato53@gmail.com>
2026-05-05 19:51:56 -0300

    fix: scale game Webview

--- src/pages/GameMaria/GameMaria.js
+++ src/pages/GameMaria/GameMaria.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function GameMaria() {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <WebView
-                source={{ uri: 'https://gx.games/games/tg3q14/maria-ecogame/' }}
-                javaScriptEnabled={true}
-                domStorageEnabled={true}
-            />
-        </SafeAreaView>
-    );
+
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+
+    return () => {
+      ScreenOrientation.unlockAsync(); 
+    };
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView 
+        source={{ uri: 'https://mar-ia-eco-game.vercel.app/' }}
+        style={{ flex: 1 }}
+      />
+    </SafeAreaView>
+  );
 }
